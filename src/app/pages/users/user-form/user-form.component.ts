3be96bc2a5647899ce4dcb9004f4929ed883747f
Michel Marinho <michel.marinho04@outlook.com>
2022-07-19 15:28:12 -0400

    Dev: UserFormComponent

--- src/app/pages/users/user-form/user-form.component.ts
+++ src/app/pages/users/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
+  userForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) {
+    this.userForm = this.fb.group({
+      id: 0,
+      nome: '',
+      sobrenome: '',
+      idade: '',
+      profissao: ''
+    })
+
+  }
+
+  createUser(){
+    console.log(this.userForm.value);
+  }
 
   ngOnInit(): void {
   }
